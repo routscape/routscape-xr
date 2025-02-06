dd2f4f0da6f60630073a1a8ea0289f5860df56d8
routscape-user <routscape-git.commits@dwigoric.dev>
2025-02-06 16:21:17 +0800

    chore: setup instantiation

--- Assets/Scenes/Pinning Scene/Select Handler.cs
+++ Assets/Scenes/Pinning Scene/Select Handler.cs
@@ -1,10 +1,17 @@
 using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
 using Oculus.Interaction.Input;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SelectHandler : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject _pinObject;
+    [SerializeField] private Transform _leftPinchArea;
+    [SerializeField] private Transform _rightPinchArea;
+    [SerializeField] private HandGrabInteractor _leftHandGrabInteractor;
+    [SerializeField] private HandGrabInteractor _rightHandGrabInteractor;
     private bool _clicked = false;
     void Start()
     {
@@ -17,16 +24,37 @@ public class SelectHandler : MonoBehaviour
         
     }
 
-    public void onClick(PointerEvent eventData)
+    private void SpawnPin()
+    {
+        var instantiatedPin = Instantiate(_pinObject, _rightPinchArea.position, quaternion.identity);
+        Debug.Log("Interactor state: " + _rightHandGrabInteractor.State);
+        HandGrabInteractable pinGrabbable = instantiatedPin.GetComponentInChildren<HandGrabInteractable>();
+        if (pinGrabbable == null)
+        {
+            Debug.Log("Hand grab interactable component missing!");
+            return;
+        }
+        /*
+         * Problem: Interactor is disabled when I select the button for some odd reason
+         *
+         * Investigate if other interactors can affect the state of other interactors.
+         */
+        
+        instantiatedPin.SetActive(true);
+      
+        Debug.Log("Pin state: " + pinGrabbable.State);
+    }
+
+    public void OnClick(PointerEvent eventData)
     {
         if (_clicked)
         {
             _clicked = false;
             return;
         }
-
-        HandRef handData = (HandRef)eventData.Data;
         _clicked = true;
+        HandRef handData = (HandRef)eventData.Data;
         Debug.Log(handData.Handedness);
+        SpawnPin();
     }
 }
