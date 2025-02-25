b819e6114fd86ed01cf46799312c18f3f2b5890c
Melonator <johndayll_arizala@dlsu.edu.ph>
2025-02-25 20:31:45 +0800

    feat: spawn pin ui on confirm (incomplete)

--- Assets/Scripts/EnableGameObject.cs
+++ Assets/Scripts/EnableGameObject.cs
@@ -1,11 +1,22 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EnableGameObject : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    [SerializeField] private GameObject pinUI;
+    [SerializeField] private Transform mapLocation;
 
+
+    private void SpawnUI()
+    {
+        var newTransform = mapLocation;
+        newTransform.position = new Vector3(newTransform.position.x, newTransform.position.y, newTransform.position.z - 0.5f);
+        Instantiate(pinUI, newTransform.position, quaternion.identity);
+    }
     public void OnTrigger()
     {
         target.SetActive(true);
+        SpawnUI();
     }
 }
