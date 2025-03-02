9927a899fe4cfec753e352055094e02b2ba7ea10
Joshua Permito <git.commits@dwigoric.dev>
2025-03-02 10:47:11 +0800

    feat: implement photon handler + room joining

---
+++ Assets/Scripts/Collab/PhotonHandler.cs
@@ -0,0 +1,58 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace Collab
+{
+    public class PhotonHandler : MonoBehaviourPunCallbacks
+    {
+        private const string GameVersion = "1";
+        [SerializeField] private byte maxPlayers = 2;
+
+        private void Awake()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        private void Start()
+        {
+            Connect();
+        }
+
+        private void Connect()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = GameVersion;
+            }
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("[PhotonHandler] Connected to Master");
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("[PhotonHandler] No room available, creating a new room");
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayers });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("[PhotonHandler] Joined room");
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("[PhotonHandler] Disconnected due to: {0}", cause);
+            ;
+        }
+    }
+}
