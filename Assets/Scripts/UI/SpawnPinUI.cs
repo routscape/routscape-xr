da4dbaeb184a31bf7bd119506aef4db06aff0363
Joshua Permito <git.commits@dwigoric.dev>
2025-03-07 11:36:24 +0800

    refactor: restructure scripts and zoom handler

---
+++ Assets/Scripts/UI/SpawnPinUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SpawnPinUI : MonoBehaviour
+{
+    [SerializeField] private GameObject pinUI;
+    [SerializeField] private Transform mapLocation;
+
+    public void SpawnUI()
+    {
+        var newPosition = mapLocation.position;
+        newPosition = new Vector3(newPosition.x, newPosition.y + 0.3f, newPosition.z);
+        pinUI.SetActive(true);
+        pinUI.transform.position = newPosition;
+    }
+}
