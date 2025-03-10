6189d90f1e54d97ec221c2ffa0372e72b1bf3275
kaninRice <escopetese@gmail.com>
2025-03-11 00:11:55 +0800

    feat: add multiplayer room ui

---
+++ Assets/Scripts/MultiplayerWindowManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MultiplayerWindowManager : MonoBehaviour
+{
+    [SerializeField] private GameObject labelInput;
+    [SerializeField] private Button joinButton;
+    [SerializeField] private Button createButton;
+
+    void Start()
+    {
+        joinButton.onClick.AddListener(JoinRoom);
+        createButton.onClick.AddListener(CreateRoom);
+    }
+
+    void JoinRoom()
+    {
+        var roomId = labelInput.transform.Find("Text Area/Placeholder");
+        Debug.Log(roomId);
+        
+        // TODO
+    }
+
+    void CreateRoom()
+    {
+        var roomId = labelInput.transform.Find("Text Area/Placeholder");
+        Debug.Log(roomId);
+        
+        // TODO
+    }
+}
