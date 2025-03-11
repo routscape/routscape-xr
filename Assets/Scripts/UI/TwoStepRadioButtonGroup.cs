828439b9329421e73cf7d045a46c0c36abe2b9a5
Joshua Permito <git.commits@dwigoric.dev>
2025-03-11 16:48:15 +0800

    feat: implement synchronized pin selection

--- Assets/Scripts/UI/TwoStepRadioButtonGroup.cs
+++ Assets/Scripts/UI/TwoStepRadioButtonGroup.cs
@@ -1,93 +1,88 @@
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 using System.Collections;
 using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class TwoStepRadioButtonGroup : MonoBehaviour
+public class TwoStepRadioButtonGroup : NetworkBehaviour
 {
-    [SerializeField] private List<Button> buttons = new List<Button>();
-    private Color defaultColor = new Color(0f, 0f, 0f, 0f);
-    private Color selectedColor = new Color(0f, 0f, 0f, 81f / 255f);
-    private Color activeColor = new Color(10f / 255f, 132f / 255f, 1f, 180f / 255f);
-	private Color drawingColor = new Color(0.31f, 0.88f, 0.28f, 180f / 255f);
-    
-	[SerializeField] private UserInterfaceManagerScript userInterfaceManager;
+    [SerializeField] private List<Button> buttons = new();
 
-    private Button selectedButton;
-	private Button activeButton;
+    [SerializeField] private UserInterfaceManagerScript userInterfaceManager;
+    private readonly Color activeColor = new(10f / 255f, 132f / 255f, 1f, 180f / 255f);
+    private readonly Color defaultColor = new(0f, 0f, 0f, 0f);
+    private readonly Color drawingColor = new(0.31f, 0.88f, 0.28f, 180f / 255f);
+    private readonly Color selectedColor = new(0f, 0f, 0f, 81f / 255f);
+    private Button activeButton;
+    private Button clickedButton;
     private bool isClickAllowed = true;
 
-	public void AddButton(Button button, bool isDrawing)
-	{
-		buttons.Add(button);
-		button.onClick.AddListener(() => OnButtonClicked(button));
+    private Button selectedButton;
 
-		if (isDrawing)
-		{
-			UpdateButtonColor(button, drawingColor);
-		} else
-		{
-			UpdateButtonColor(button, defaultColor);
-		}
-	}
+    public void AddButton(Button button, bool isDrawing)
+    {
+        buttons.Add(button);
+        button.onClick.AddListener(() => OnButtonClicked(button));
 
-	public void RemoveAllButton()
-	{
-		buttons.Clear();
-	}
+        if (isDrawing)
+            UpdateButtonColor(button, drawingColor);
+        else
+            UpdateButtonColor(button, defaultColor);
+    }
 
-	public void SetNoActive()
-	{
-		if (selectedButton != null)
-		{
-			UpdateButtonColor(selectedButton, defaultColor);
-		}
+    public void RemoveAllButton()
+    {
+        buttons.Clear();
+    }
 
-		if (activeButton != null)
-		{
-			UpdateButtonColor(activeButton, defaultColor);
-		}
+    public void SetNoActive()
+    {
+        if (selectedButton != null) UpdateButtonColor(selectedButton, defaultColor);
 
-		selectedButton = null;
-		activeButton = null;
-	}
+        if (activeButton != null) UpdateButtonColor(activeButton, defaultColor);
 
-    private void OnButtonClicked(Button clickedButton)
+        selectedButton = null;
+        activeButton = null;
+    }
+
+    private void OnButtonClicked(Button clickedButtonFromEvent)
+    {
+        clickedButton = clickedButtonFromEvent;
+    }
+
+    [Rpc]
+    private void RpcToggleMode()
     {
         if (!isClickAllowed) return;
         isClickAllowed = false;
         StartCoroutine(EnableClickAfterDelay(0.3f));
 
-		/* disable button click if drawing route */
-		if (userInterfaceManager.currentActiveRoute != null) return;
+        /* disable button click if drawing route */
+        if (userInterfaceManager.currentActiveRoute != null) return;
 
         if (clickedButton == selectedButton)
         {
             Debug.Log("Selected button clicked");
-			selectedButton = null;
-			activeButton = clickedButton;
-			userInterfaceManager.OpenEditWindow(clickedButton);
+            selectedButton = null;
+            activeButton = clickedButton;
+            userInterfaceManager.OpenEditWindow(clickedButton);
             UpdateButtonColor(clickedButton, activeColor);
         }
-		else if (clickedButton == activeButton) 
-		{
-			Debug.Log("Active button clicked");
-			activeButton = null;
-			UpdateButtonColor(clickedButton, defaultColor);
-		}
+        else if (clickedButton == activeButton)
+        {
+            Debug.Log("Active button clicked");
+            activeButton = null;
+            UpdateButtonColor(clickedButton, defaultColor);
+        }
         else
         {
-			userInterfaceManager.CloseEditWindow();
+            userInterfaceManager.CloseEditWindow();
             selectedButton = clickedButton;
-            
-			var geoData = selectedButton.GetComponent<UIGeodata>();
-			if (geoData != null) userInterfaceManager.JumpTo(geoData.latLong);
 
-            foreach (Button button in buttons)
-            {
-                UpdateButtonColor(button, defaultColor);
-            }
+            var geoData = selectedButton.GetComponent<UIGeodata>();
+            if (geoData != null) userInterfaceManager.JumpTo(geoData.latLong);
+
+            foreach (var button in buttons) UpdateButtonColor(button, defaultColor);
 
             UpdateButtonColor(clickedButton, selectedColor);
         }
@@ -101,7 +96,7 @@ public class TwoStepRadioButtonGroup : MonoBehaviour
 
     private void UpdateButtonColor(Button button, Color color)
     {
-        Image buttonImage = button.GetComponent<Image>();
+        var buttonImage = button.GetComponent<Image>();
         if (buttonImage != null)
         {
             Debug.Log($"Updating {button.name} color: {color}");
