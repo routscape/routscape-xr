f0e7d0d922952ec98e6a41473eac743c908130b0
Joshua Permito <git.commits@dwigoric.dev>
2025-03-11 13:19:37 +0800

    feat: implement UI-based matchmaking with room creation and joining

--- Assets/Scripts/Collab/MultiplayerWindowManager.cs
+++ Assets/Scripts/Collab/MultiplayerWindowManager.cs
@@ -1,3 +1,4 @@
+using Meta.XR.MultiplayerBlocks.Shared;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,41 +10,77 @@ namespace Collab
         [SerializeField] private GameObject labelInput;
         [SerializeField] private Button joinButton;
         [SerializeField] private Button createButton;
+        [SerializeField] private CustomMatchmaking customMatchmaking;
 
-        void Start()
+        private readonly string _roomPassword = "r0utsc4p3";
+
+        private void Start()
         {
             joinButton.onClick.AddListener(JoinRoom);
             createButton.onClick.AddListener(CreateRoom);
         }
 
-        void JoinRoom()
+        private async void JoinRoom()
         {
             var roomId = labelInput.GetComponent<TMP_InputField>().text;
             Debug.Log("RoomID: " + roomId);
-        
-            // TODO
-            SetRoomId(roomId);
+
             DisableInput();
+            SetInputTextField("Joining room...");
+
+            var joinResult = await customMatchmaking.JoinRoom(roomId, _roomPassword);
+            if (joinResult.IsSuccess)
+            {
+                SetInputTextField(roomId);
+            }
+            else
+            {
+                Debug.LogError("Failed to join room");
+                SetInputTextField("Failed to join room");
+                EnableInput();
+            }
         }
 
-        void CreateRoom()
+        private async void CreateRoom()
         {
-            var roomId = labelInput.GetComponent<TMP_InputField>().text;
-            Debug.Log("RoomID: " + roomId);
-        
-            // TODO
-            SetRoomId(roomId);
             DisableInput();
+            SetInputTextField("Creating room...");
+
+            var createResult = await customMatchmaking.CreateRoom(new CustomMatchmaking.RoomCreationOptions
+            {
+                IsPrivate = false,
+                LobbyName = "MultiplayerSession",
+                MaxPlayersPerRoom = 2,
+                RoomPassword = _roomPassword
+            });
+
+            if (createResult.IsSuccess)
+            {
+                var roomId = createResult.RoomToken;
+                Debug.Log("RoomID: " + roomId);
+                SetInputTextField(roomId);
+            }
+            else
+            {
+                Debug.LogError("Failed to create room");
+                SetInputTextField("Failed to create room");
+                EnableInput();
+            }
         }
 
-        void SetRoomId(string roomId)
+        private void SetInputTextField(string textValue)
         {
-            labelInput.GetComponent<TMP_InputField>().text = roomId;
+            labelInput.GetComponent<TMP_InputField>().text = textValue;
         }
 
-        void DisableInput()
+        private void DisableInput()
         {
             labelInput.GetComponent<TMP_InputField>().interactable = false;
+        }
+
+        private void EnableInput()
+        {
+            labelInput.GetComponent<TMP_InputField>().interactable = true;
         }
     }
 }
