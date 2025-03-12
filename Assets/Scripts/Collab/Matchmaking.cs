9db2f54f779887c88694dbddae8f09204c4b473f
Joshua Permito <git.commits@dwigoric.dev>
2025-03-12 14:47:39 +0800

    feat: call network runner's game start function

---
+++ Assets/Scripts/Collab/Matchmaking.cs
@@ -0,0 +1,16 @@
+﻿using Fusion;
+using UnityEngine;
+
+namespace Collab
+{
+    public class Matchmaking : MonoBehaviour
+    {
+        public void OnFusionServerConnected(NetworkRunner networkRunner)
+        {
+            networkRunner.StartGame(new StartGameArgs
+            {
+                SessionName = "MultiplayerSession"
+            });
+        }
+    }
+}
