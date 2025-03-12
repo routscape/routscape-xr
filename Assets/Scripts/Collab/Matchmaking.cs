1b327bedc0f7c0f69fb719ea5cfe276228889550
Joshua Permito <git.commits@dwigoric.dev>
2025-03-12 16:39:54 +0800

    Revert "feat: attempt automated custom matchmaking"
    This reverts commit 7eab73a94d43362dfd9c0b15874dee7c42651182.

--- Assets/Scripts/Collab/Matchmaking.cs
+++ Assets/Scripts/Collab/Matchmaking.cs
@@ -1,16 +1,25 @@
-﻿using Meta.XR.MultiplayerBlocks.Shared;
+﻿using Fusion;
+using Meta.XR.MultiplayerBlocks.Shared;
 using UnityEngine;
-using Behaviour = Fusion.Behaviour;
 
 namespace Collab
 {
-    public class Matchmaking : Behaviour
+    public class Matchmaking : MonoBehaviour
     {
         [SerializeField] private CustomMatchmaking customMatchmaking;
 
         private void OnApplicationQuit()
         {
             customMatchmaking.LeaveRoom();
+        }
+
+        public void OnFusionServerConnected(NetworkRunner networkRunner)
+        {
+            if (networkRunner.GetComponent<INetworkSceneManager>() == null)
+                networkRunner.gameObject.AddComponent<NetworkSceneManagerDefault>();
+
+            if (networkRunner.GetComponent<INetworkObjectProvider>() == null)
+                networkRunner.gameObject.AddComponent<NetworkObjectProviderDefault>();
         }
     }
 }
