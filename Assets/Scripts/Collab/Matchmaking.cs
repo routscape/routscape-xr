2ed88e456c8cb46c5277cc98b6304b0ac8ea6fc2
Joshua Permito <git.commits@dwigoric.dev>
2025-03-12 14:54:03 +0800

    feat: leave room on application quit

--- Assets/Scripts/Collab/Matchmaking.cs
+++ Assets/Scripts/Collab/Matchmaking.cs
@@ -1,10 +1,18 @@
 ﻿using Fusion;
+using Meta.XR.MultiplayerBlocks.Shared;
 using UnityEngine;
 
 namespace Collab
 {
     public class Matchmaking : MonoBehaviour
     {
+        [SerializeField] private CustomMatchmaking customMatchmaking;
+
+        private void OnApplicationQuit()
+        {
+            customMatchmaking.LeaveRoom();
+        }
+
         public void OnFusionServerConnected(NetworkRunner networkRunner)
         {
             networkRunner.StartGame(new StartGameArgs
