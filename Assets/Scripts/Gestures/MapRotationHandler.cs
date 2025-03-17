b00e83c6ce676c65f3cb2e159ab8bef9a7e59edf
Joshua Permito <git.commits@dwigoric.dev>
2025-03-17 15:14:56 +0800

    fix: request state authority when rotating

--- Assets/Scripts/Gestures/MapRotationHandler.cs
+++ Assets/Scripts/Gestures/MapRotationHandler.cs
@@ -1,13 +1,18 @@
 using Fusion;
+using Oculus.Interaction;
 using UnityEngine;
 
 namespace Gestures
 {
     public class MapRotationHandler : NetworkBehaviour
     {
+        [SerializeField] private RayInteractor leftRayInteractor;
+        [SerializeField] private RayInteractor rightRayInteractor;
         [SerializeField] private Transform monitoredTransform;
 
         private bool _isSpawned;
+        private bool _leftPositionSet;
+        private bool _rightPositionSet;
 
         [Networked]
         [OnChangedRender(nameof(UpdateRotation))]
@@ -33,6 +38,50 @@ namespace Gestures
                 Debug.Log("[MapRotationHandler] Updating rotation to " + CurrentRotation);
                 monitoredTransform.rotation = CurrentRotation;
             }
+        }
+
+        public void OnSelect(PointerEvent pointerEvent)
+        {
+            if (pointerEvent.Type != PointerEventType.Select)
+            {
+                Debug.LogWarning("[MapRotationHandler] Ignoring due to unexpected pointer event type: " +
+                                 pointerEvent.Type);
+                return;
+            }
+
+            var rayInteractor = pointerEvent.Data as RayInteractor;
+            if (rayInteractor == null)
+            {
+                Debug.LogWarning("[MapRotationHandler] Ignoring due to unexpected pointer event data type: " +
+                                 pointerEvent.Data.GetType());
+                return;
+            }
+
+            if (rayInteractor == leftRayInteractor) _leftPositionSet = true;
+            if (rayInteractor == rightRayInteractor) _rightPositionSet = true;
+
+            if (_leftPositionSet && _rightPositionSet) Object.RequestStateAuthority();
+        }
+
+        public void OnDeselect(PointerEvent pointerEvent)
+        {
+            if (pointerEvent.Type != PointerEventType.Unselect)
+            {
+                Debug.LogWarning("[MapRotationHandler] Ignoring due to unexpected pointer event type: " +
+                                 pointerEvent.Type);
+                return;
+            }
+
+            var rayInteractor = pointerEvent.Data as RayInteractor;
+            if (rayInteractor == null)
+            {
+                Debug.LogWarning("[MapRotationHandler] Ignoring due to unexpected pointer event data type: " +
+                                 pointerEvent.Data.GetType());
+                return;
+            }
+
+            if (rayInteractor == leftRayInteractor) _leftPositionSet = false;
+            if (rayInteractor == rightRayInteractor) _rightPositionSet = false;
         }
     }
 }
