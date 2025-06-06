5e368ed5a454dcaa71bd1219ba91e1f79da2b465
Joshua Permito <git.commits@dwigoric.dev>
2025-06-06 14:17:35 +0800

    fix: correctly compute flood cube boundary

--- Assets/Scripts/Flooding/FloodCubeManager.cs
+++ Assets/Scripts/Flooding/FloodCubeManager.cs
@@ -8,7 +8,8 @@ namespace Flooding
         [SerializeField] private int gridSize = 64;
         [SerializeField] private Vector4 boundaries;
 
-        private float _cubeSize;
+        private float _cubeSizeX;
+        private float _cubeSizeZ;
 
         private void Start()
         {
@@ -17,21 +18,22 @@ namespace Flooding
 
         private void GenerateCubes()
         {
-            _cubeSize = (boundaries.w - boundaries.x) / gridSize;
+            _cubeSizeX = (boundaries.y - boundaries.x) / gridSize;
+            _cubeSizeZ = (boundaries.w - boundaries.z) / gridSize;
 
             for (var x = 0; x < gridSize; x++)
             for (var z = 0; z < gridSize; z++)
             {
                 var position = new Vector3(
-                    boundaries.x + x * _cubeSize + _cubeSize / 2,
-                    boundaries.y,
-                    boundaries.z + z * _cubeSize + _cubeSize / 2
+                    boundaries.x + x * _cubeSizeX + _cubeSizeX / 2,
+                    0,
+                    boundaries.z + z * _cubeSizeZ + _cubeSizeZ / 2
                 );
 
-                var floodCube = Instantiate(floodCubePrefab, position, Quaternion.identity);
-                floodCube.transform.localScale = new Vector3(_cubeSize, _cubeSize, _cubeSize);
-                floodCube.name = $"FloodCube_{x}_{z}";
-                floodCube.transform.SetParent(transform);
+                var cube = Instantiate(floodCubePrefab, position, Quaternion.identity);
+                cube.transform.localScale = new Vector3(_cubeSizeX, 1, _cubeSizeZ);
+                cube.name = $"FloodCube_{x}_{z}";
+                cube.transform.SetParent(transform);
             }
         }
     }
