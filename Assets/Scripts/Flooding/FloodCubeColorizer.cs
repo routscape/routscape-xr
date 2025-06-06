a49284633ea8b63f552e2ce6496713e9d0061a69
Joshua Permito <git.commits@dwigoric.dev>
2025-06-06 19:01:12 +0800

    fix(flood cubes): correctly compute distance between mesh and cube max Y

--- Assets/Scripts/Flooding/FloodCubeColorizer.cs
+++ Assets/Scripts/Flooding/FloodCubeColorizer.cs
@@ -6,9 +6,11 @@ namespace Flooding
     public class FloodCubeColorizer : MonoBehaviour
     {
         [SerializeField] private float alpha = 0.5f;
+        private Bounds _bounds;
 
         private float _distanceToMap;
         private AbstractMap _map;
+        private Renderer _renderer;
         private Color _transparentGreen;
         private Color _transparentRed;
 
@@ -16,6 +18,8 @@ namespace Flooding
         {
             _transparentGreen = new Color(0, 1, 0, alpha);
             _transparentRed = new Color(1, 0, 0, alpha);
+            _renderer = GetComponent<Renderer>();
+            _bounds = _renderer.bounds;
         }
 
         private void FixedUpdate()
@@ -28,9 +32,10 @@ namespace Flooding
             }
 
             var position = transform.position;
-            var cubeLatLon = _map.WorldToGeoPosition(position);
+            var topYPosition = new Vector3(position.x, _bounds.max.y, position.z);
+            var cubeLatLon = _map.WorldToGeoPosition(topYPosition);
             var mapPosition = _map.GeoToWorldPosition(cubeLatLon);
-            _distanceToMap = Vector3.Distance(position, mapPosition);
+            _distanceToMap = Vector3.Distance(topYPosition, mapPosition);
         }
 
         private void LateUpdate()
@@ -45,10 +50,7 @@ namespace Flooding
             var color = Color.Lerp(_transparentRed, _transparentGreen, _distanceToMap / 100f);
 
             var renderer = GetComponent<Renderer>();
-            if (renderer != null)
-                renderer.material.SetColor("_Color", color);
-            else
-                Debug.LogWarning($"Renderer not found on {gameObject.name}. Ensure it has a Renderer component.");
+            renderer.material.SetColor("_Color", color);
         }
 
         public void InitializeMap(AbstractMap map)
