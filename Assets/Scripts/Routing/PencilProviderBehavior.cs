de4bf3081b4a2d5fe96155de671c249c688aca8f
Kovie Niño <thekovie@gmail.com>
2025-06-06 13:32:07 +0800

    feat: RPC hidepencil onroutebegin

--- Assets/Scripts/Routing/PencilProviderBehavior.cs
+++ Assets/Scripts/Routing/PencilProviderBehavior.cs
@@ -11,28 +11,43 @@ public class PencilProviderBehavior : MonoBehaviour
     private NetworkEventDispatcher _networkEventDispatcher;
     private Transform _pinchPoint;
     private GameObject _instantiatedPencil;
+
     void Start()
     {
-        _networkEventDispatcher = GameObject.FindWithTag("network event dispatcher").GetComponent<NetworkEventDispatcher>();
-        if (_networkEventDispatcher== null)
+        _networkEventDispatcher =
+            GameObject.FindWithTag("network event dispatcher").GetComponent<NetworkEventDispatcher>();
+        if (_networkEventDispatcher == null)
         {
             Debug.Log("[PinDropper] No network event dispatcher found!");
             throw new Exception("[PinDropper] No network event dispatcher found!");
         }
+
         _pinchPoint = GameObject.FindWithTag("right pinch area").transform;
+        _networkEventDispatcher.OnRouteBegin += HidePencil;
+        _networkEventDispatcher.OnRouteEnd += ShowPencil;
     }
+
     public void OnSelect(PointerEvent eventData)
     {
         _instantiatedPencil = Instantiate(pencilPrefab, _pinchPoint);
         _instantiatedPencil.transform.localPosition = new Vector3(0, -0.00630000001f, -0.00889999978f);
-        meshRenderer.enabled = false;
-        boxCollider.enabled = false;
     }
-    
+
     public void OnRelease(PointerEvent eventData)
     {
         _instantiatedPencil.Destroy();
+    }
+
+    private void HidePencil(string routeName, int objectCategory, int colorType)
+    {
+        meshRenderer.enabled = false;
+        boxCollider.enabled = false;
+    }
+
+    private void ShowPencil()
+    {
         meshRenderer.enabled = true;
         boxCollider.enabled = true;
     }
+
 }
