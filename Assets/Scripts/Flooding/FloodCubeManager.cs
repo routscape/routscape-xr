0a83b5d7a05bb067c91e84536fffec182fdff4d2
Joshua Permito <git.commits@dwigoric.dev>
2025-06-10 13:48:21 +0800

    chore: change default grid size to 10

--- Assets/Scripts/Flooding/FloodCubeManager.cs
+++ Assets/Scripts/Flooding/FloodCubeManager.cs
@@ -1,9 +1,7 @@
 ﻿using Mapbox.Map;
 using Mapbox.Unity.Map;
-using Mapbox.Unity.MeshGeneration.Data;
 using Mapbox.Unity.Utilities;
 using Mapbox.Utils;
-using Unity.Collections;
 using UnityEngine;
 
 namespace Flooding
@@ -12,30 +10,34 @@ namespace Flooding
     {
         [SerializeField] private GameObject floodCubePrefab;
         [SerializeField] private AbstractMap mapManager;
-        [SerializeField] private int gridSize = 64;
+        [SerializeField] private int gridSize = 10;
         [SerializeField] private Vector4 boundaries;
         [SerializeField] private float startingFloodHeight = 1f;
+
         [SerializeField] private float startingFloodYScale = 0.25f;
+
         //Distance threshold in centimeters
         [SerializeField] private float lowFloodThreshold = 0.02f;
         [SerializeField] private float highFloodThreshold = 0.05f;
-        [Header("Flood Colors")]
-        [SerializeField] private Color green;
+
+        [Header("Flood Colors")] [SerializeField]
+        private Color green;
+
         [SerializeField] private Color yellow;
         [SerializeField] private Color red;
 
-        private FloodCube[] _floodCubes;
-        private float[] _mapHeights;
-        
         private float _cubeSizeX;
         private float _cubeSizeZ;
 
+        private FloodCube[] _floodCubes;
+        private float[] _mapHeights;
+
         private void Start()
         {
-             _floodCubes = new FloodCube[gridSize * gridSize];
-             _mapHeights = new float[gridSize * gridSize];
-             GenerateCubes(); 
-             mapManager.OnUpdated += Init;
+            _floodCubes = new FloodCube[gridSize * gridSize];
+            _mapHeights = new float[gridSize * gridSize];
+            GenerateCubes();
+            mapManager.OnUpdated += Init;
         }
 
         private void Init()
@@ -49,7 +51,7 @@ namespace Flooding
             _cubeSizeX = (boundaries.y - boundaries.x) / gridSize;
             _cubeSizeZ = (boundaries.w - boundaries.z) / gridSize;
 
-            int index = 0;
+            var index = 0;
             for (var x = 0; x < gridSize; x++)
             for (var z = 0; z < gridSize; z++)
             {
@@ -72,9 +74,9 @@ namespace Flooding
         //TODO: Use bounds instead of transform's position...
         private void SetCubeColors()
         {
-            for (int i = 0; i < gridSize * gridSize; i++)
+            for (var i = 0; i < gridSize * gridSize; i++)
             {
-                Color color = new Color();
+                var color = new Color();
                 var distance = Mathf.Abs(_floodCubes[i].transform.position.y - _mapHeights[i]);
                 if (distance <= lowFloodThreshold)
                 {
@@ -92,14 +94,14 @@ namespace Flooding
                 {
                     color = red;
                 }
-              
+
                 _floodCubes[i].SetColor(color);
             }
         }
 
         private void GetMapHeights()
         {
-            for (int i = 0; i < _floodCubes.Length; i++)
+            for (var i = 0; i < _floodCubes.Length; i++)
             {
                 var latLong = mapManager.WorldToGeoPosition(_floodCubes[i].transform.position);
                 _mapHeights[i] = GetHeightFromLatLong(latLong);
@@ -110,17 +112,17 @@ namespace Flooding
         {
             //Find the tile given latlong
             var tileIDUnwrapped = TileCover.CoordinateToTileId(latlong, (int)mapManager.Zoom);
-            UnityTile tile = mapManager.MapVisualizer.GetUnityTileFromUnwrappedTileId(tileIDUnwrapped);
+            var tile = mapManager.MapVisualizer.GetUnityTileFromUnwrappedTileId(tileIDUnwrapped);
 
-            Vector2d v2d = Conversions.LatLonToMeters(latlong);
-            Vector2d v2dcenter = tile.Rect.Center - new Mapbox.Utils.Vector2d(tile.Rect.Size.x / 2, tile.Rect.Size.y / 2);
-            Vector2d diff = v2d - v2dcenter;
+            var v2d = Conversions.LatLonToMeters(latlong);
+            var v2dcenter = tile.Rect.Center - new Vector2d(tile.Rect.Size.x / 2, tile.Rect.Size.y / 2);
+            var diff = v2d - v2dcenter;
 
-            float Dx = (float)(diff.x / tile.Rect.Size.x);
-            float Dy = (float)(diff.y / tile.Rect.Size.y);
+            var Dx = (float)(diff.x / tile.Rect.Size.x);
+            var Dy = (float)(diff.y / tile.Rect.Size.y);
 
             //height in unity units
-            return tile.QueryHeightData(Dx,Dy);
+            return tile.QueryHeightData(Dx, Dy);
         }
     }
 }
