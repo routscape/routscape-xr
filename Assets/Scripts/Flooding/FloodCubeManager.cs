eb16a3352c6bb8a89d4d2b0331bd8187ba9e6612
Melonator <johndayll_arizala@dlsu.edu.ph>
2025-06-10 16:10:23 +0800

    feat: initial optimized flood cubes

--- Assets/Scripts/Flooding/FloodCubeManager.cs
+++ Assets/Scripts/Flooding/FloodCubeManager.cs
@@ -1,7 +1,10 @@
-﻿using Mapbox.Map;
+﻿using Gestures;
+using Mapbox.Map;
 using Mapbox.Unity.Map;
+using Mapbox.Unity.MeshGeneration.Data;
 using Mapbox.Unity.Utilities;
 using Mapbox.Utils;
+using Unity.Collections;
 using UnityEngine;
 
 namespace Flooding
@@ -10,38 +13,44 @@ namespace Flooding
     {
         [SerializeField] private GameObject floodCubePrefab;
         [SerializeField] private AbstractMap mapManager;
-        [SerializeField] private int gridSize = 10;
+        [SerializeField] private MapZoomHandler mapZoomHandler;
+        [SerializeField] private GestureManager gestureManager;
+        [SerializeField] private int gridSize = 64;
         [SerializeField] private Vector4 boundaries;
-        [SerializeField] private float startingFloodHeight = 1f;
-
         [SerializeField] private float startingFloodYScale = 0.25f;
-
         //Distance threshold in centimeters
         [SerializeField] private float lowFloodThreshold = 0.02f;
         [SerializeField] private float highFloodThreshold = 0.05f;
-
-        [Header("Flood Colors")] [SerializeField]
-        private Color green;
-
+        [Header("Flood Colors")]
+        [SerializeField] private Color green;
         [SerializeField] private Color yellow;
         [SerializeField] private Color red;
 
+        public float floodHeight = 3000f;
+
+        private FloodCube[] _floodCubes;
+        private Vector3[] _planePositions; //plane heights with respect to the map local space
+        private float[] _mapHeights; //map heights with respect to its own local space
+        
         private float _cubeSizeX;
         private float _cubeSizeZ;
 
-        private FloodCube[] _floodCubes;
-        private float[] _mapHeights;
-
         private void Start()
         {
-            _floodCubes = new FloodCube[gridSize * gridSize];
-            _mapHeights = new float[gridSize * gridSize];
-            GenerateCubes();
-            mapManager.OnUpdated += Init;
+             _floodCubes = new FloodCube[gridSize * gridSize];
+             _mapHeights = new float[gridSize * gridSize];
+             _planePositions = new Vector3[gridSize * gridSize];
+             GenerateCubes();
+             ReScaleHeight();
+             GetCubeHeights();
+             mapZoomHandler.OnZoom += ReScaleHeight;
+             gestureManager.OnGestureEnd += RenderCubes;
+             gestureManager.OnGestureEnd += ReScaleHeight;
         }
 
-        private void Init()
+        private void RenderCubes()
         {
+            GetCubeHeights();
             GetMapHeights();
             SetCubeColors();
         }
@@ -51,33 +60,67 @@ namespace Flooding
             _cubeSizeX = (boundaries.y - boundaries.x) / gridSize;
             _cubeSizeZ = (boundaries.w - boundaries.z) / gridSize;
 
-            var index = 0;
+            int index = 0;
             for (var x = 0; x < gridSize; x++)
             for (var z = 0; z < gridSize; z++)
             {
                 var position = new Vector3(
                     boundaries.x + x * _cubeSizeX + _cubeSizeX / 2,
-                    startingFloodHeight,
+                    0,
                     boundaries.z + z * _cubeSizeZ + _cubeSizeZ / 2
                 );
 
                 var cube = Instantiate(floodCubePrefab, position, Quaternion.identity);
-                cube.transform.localScale = new Vector3(_cubeSizeX, startingFloodYScale, _cubeSizeZ);
+                cube.transform.localScale = new Vector3(_cubeSizeX, 1, _cubeSizeZ);
                 cube.name = $"FloodCube_{x}_{z}";
-                cube.transform.SetParent(transform);
+                cube.transform.SetParent(transform, true);
+                //Bring back position to localspace Y=0
+                cube.transform.localPosition = new Vector3(cube.transform.localPosition.x, 0, cube.transform.localPosition.z);
                 _floodCubes[index] = cube.GetComponent<FloodCube>();
                 _floodCubes[index].enabled = true;
                 index++;
             }
         }
 
+        private void GetCubeHeights()
+        {
+            for (int i = 0; i < gridSize * gridSize; i++)
+            {
+                var plane = _floodCubes[i];
+                var planePosition = new Vector3(plane.transform.position.x, transform.position.y,
+                    plane.transform.position.z);
+            
+                _planePositions[i] = planePosition;
+            }
+        }
+        
+        private void GetMapHeights()
+        {
+            for (int i = 0; i < _floodCubes.Length; i++)
+            {
+                var latLong = mapManager.WorldToGeoPosition(_floodCubes[i].transform.position);
+                _mapHeights[i] = mapManager.GeoToWorldPosition(latLong).y;
+            }
+        }
+
+        
+        //Method when map zooms in
+        private void ReScaleHeight()
+        {
+            RectD referenceTileRect = Conversions.TileBounds(TileCover.CoordinateToTileId(mapManager.CenterLatitudeLongitude, mapManager.AbsoluteZoom));
+            double zoomDifference = mapManager.Zoom - mapManager.AbsoluteZoom;
+            double floodLevelMeters = floodHeight / 100;
+            double unitsPerMeter = (mapManager.Options.scalingOptions.unityTileSize / referenceTileRect.Size.x) * Mathd.Pow(2d, zoomDifference);
+            transform.localPosition = new Vector3(transform.localPosition.x, (float)(floodLevelMeters * unitsPerMeter), transform.localPosition.z);
+        }
+
         //TODO: Use bounds instead of transform's position...
         private void SetCubeColors()
         {
-            for (var i = 0; i < gridSize * gridSize; i++)
+            for (int i = 0; i < gridSize * gridSize; i++)
             {
-                var color = new Color();
-                var distance = Mathf.Abs(_floodCubes[i].transform.position.y - _mapHeights[i]);
+                Color color = new Color();
+                var distance = Mathf.Abs(_planePositions[i].y - _mapHeights[i]);
                 if (distance <= lowFloodThreshold)
                 {
                     // Smooth gradient from green to yellow
@@ -94,17 +137,9 @@ namespace Flooding
                 {
                     color = red;
                 }
-
+              
                 _floodCubes[i].SetColor(color);
-            }
-        }
-
-        private void GetMapHeights()
-        {
-            for (var i = 0; i < _floodCubes.Length; i++)
-            {
-                var latLong = mapManager.WorldToGeoPosition(_floodCubes[i].transform.position);
-                _mapHeights[i] = GetHeightFromLatLong(latLong);
+                _floodCubes[i].SetText(_planePositions[i].y + ", " + _mapHeights[i]);
             }
         }
 
@@ -112,17 +147,17 @@ namespace Flooding
         {
             //Find the tile given latlong
             var tileIDUnwrapped = TileCover.CoordinateToTileId(latlong, (int)mapManager.Zoom);
-            var tile = mapManager.MapVisualizer.GetUnityTileFromUnwrappedTileId(tileIDUnwrapped);
+            UnityTile tile = mapManager.MapVisualizer.GetUnityTileFromUnwrappedTileId(tileIDUnwrapped);
 
-            var v2d = Conversions.LatLonToMeters(latlong);
-            var v2dcenter = tile.Rect.Center - new Vector2d(tile.Rect.Size.x / 2, tile.Rect.Size.y / 2);
-            var diff = v2d - v2dcenter;
+            Vector2d v2d = Conversions.LatLonToMeters(latlong);
+            Vector2d v2dcenter = tile.Rect.Center - new Mapbox.Utils.Vector2d(tile.Rect.Size.x / 2, tile.Rect.Size.y / 2);
+            Vector2d diff = v2d - v2dcenter;
 
-            var Dx = (float)(diff.x / tile.Rect.Size.x);
-            var Dy = (float)(diff.y / tile.Rect.Size.y);
+            float Dx = (float)(diff.x / tile.Rect.Size.x);
+            float Dy = (float)(diff.y / tile.Rect.Size.y);
 
             //height in unity units
-            return tile.QueryHeightData(Dx, Dy);
+            return tile.QueryHeightData(Dx,Dy);
         }
     }
 }
